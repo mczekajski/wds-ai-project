064e347ebabaa6642ffde71cdb6744b23c2ed9ce
Marcin Czekajski <czekajskimarcin@gmail.com>
2022-12-18 11:10:06 +0100

    html template

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 const net = new brain.NeuralNetwork();
 
-net.train([
+const data = [
   {
-    input: [0, 0],
-    output: [0],
-  },
-  {
-    input: [1, 0],
+    input: { r: 0, g: 0, b: 0 },
     output: [1],
   },
   {
-    input: [0, 1],
-    output: [1],
-  },
-  {
-    input: [1, 1],
+    input: { r: 1, g: 1, b: 1 },
     output: [0],
   },
-]);
+];
+
+net.train(data);
 
 const diagram = document.getElementById("diagram");
 diagram.innerHTML = brain.utilities.toSVG(net);
+
+console.log(net.run([0, 0]));
